3dfbf626211f83db002813c75ef2cbeed6fe92cd
Hloma Z Monama <160079404+HZMonama@users.noreply.github.com>
2026-02-08 06:11:37 +0200

    feat: update CI config and add coverage dependencies

--- src/opavm/__init__.py
+++ src/opavm/__init__.py
@@ -1,3 +1,8 @@
 """opavm package."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("opavm")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
